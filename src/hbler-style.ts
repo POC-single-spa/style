231a93cac4f42619a91f9dd9e04b827fd5157425
hugobler <hugo.ernesto@ouribank.com.br>
2024-03-11 10:41:09 -0300

    tailwind config

--- src/hbler-style.ts
+++ src/hbler-style.ts
@@ -1,3 +1,7 @@
-import "./global.css?modules=false";
+import "./local.css?modules=false";
+import "./tw.css?modules=false";
+import * as tailwindClasses from "tailwind-variants";
+
+export const tw = tailwindClasses;
 
 // Anything exported from this file is importable by other in-browser modules.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,tsx}"],
+  content: ["../*/src/**/*.{html,js,ts,tsx}"],
   theme: {
     colors: {
       primary: "var(--color-primary)",
       secondary: "var(--color-secondary)",
+      primaryContrast: "var(--color-primaryContrast)",
+      secondaryContrast: "var(--color-secondaryContrast)",
       danger: "var(--color-danger)",
       warning: "var(--color-warning)",
       info: "var(--color-info)",
